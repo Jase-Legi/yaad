146af74aa4c4e5ad9da4a17fcd17dd6cc27f8557
Jase Legi <jasonlegister@gmail.com>
2022-07-14 13:27:10 -0400

    doodoo

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const pinataSDK = require('@pinata/sdk');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({extended: true}))
+const corsOptions = {
+    "origin": 'https://yaadlabs.com',
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+    "preflightContinue": true,
+    "optionsSuccessStatus": 200
+}
 app.use(cors());
 app.use('/api',indexRoute);
 app.use('/progress',progresschecker);
