b7a4d01b53198eec90d26f07fbfda55ed346f3ef
Jase Legi <jasonlegister@gmail.com>
2022-07-15 11:04:00 -0400

    br

--- routes/index.js
+++ routes/index.js
@@ -256,8 +256,8 @@ const corsOptions = {
     "preflightContinue": true,
     "optionsSuccessStatus": 200
 }
-index.use(cors(corsOptions));
-// index.options('/', cors(corsOptions));
+index.use(cors());
+// index.options('*', cors());
 
 index.get('/', (req, res, next)=>{
     
