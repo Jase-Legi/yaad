126f77f57a31722dcbc17e94cbc7fa9df86acbce
Jase Legi <jasonlegister@gmail.com>
2022-07-15 11:14:58 -0400

    br

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 let app = express();
+const corsOptions = {
+    "origin": 'https://yaadlabs.com',
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+    "preflightContinue": true,
+    "optionsSuccessStatus": 200
+}
+// index.use(cors(corsOptions));
+app.options('*', cors());
 
 const {createReadStream} = require("fs");
 const {join, resolve} =require("path");
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const index = express();
 
 require('dotenv').config();
@@ -250,14 +250,6 @@ const ethersCreatePair = async ()=>{
 // ethersCreatePair();
 const clientUri = 'https://yaadlabs.herokuapp.com/'
 /* GET home page. */
-const corsOptions = {
-    "origin": 'https://yaadlabs.com',
-    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
-    "preflightContinue": true,
-    "optionsSuccessStatus": 200
-}
-index.use(cors(corsOptions));
-// index.options('*', cors());
 
 index.get('/', (req, res, next)=>{
     
